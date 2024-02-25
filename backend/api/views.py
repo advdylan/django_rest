bd443261b3a90cbc23fc6b0d3d29b52e3eebdbf4
advdylan <immortalizetimes@gmail.com>
2024-02-25 19:41:01 +0100

    rest api

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,9 +1,23 @@
 from django.http import JsonResponse
+import json
 
 def api_home(request, *args, **kwargs):
     #request -> HTTPRequest -> Django
     #request.body
-    print(dir(request))
+    #print(dir(request))
+    print(request.GET) #URL query params
+    print(request.POST)
     body = request.body # byte stringJSON data
-    print(body)
-    return JsonResponse({"message": "API Response for Django"})
+    data = {}
+    try:
+        data = json.loads(body) #string of JSON data -> Python dict
+    except:
+        pass
+
+    print(data)
+    
+    #print(request.headers)
+    data['params'] = dict(request.GET)
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type
+    return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -4,12 +4,9 @@ import requests
 #endpoint = "https://httpbin.org/anything"
 endpoint = "http://localhost:8000/api/" #127.0.0.1:8000
 
-get_response = requests.get(endpoint, json={"query":"hello,world"}) #HTTP Req
+get_response = requests.get(endpoint, params={"product_id":123}, json={"query":"hello,world"}) #HTTP Req
 
 #print(get_response.json())
 #print(get_response.status_code)
 print(get_response.json())
 #print(get_response.json()['message'])
-
-
-#
