03a4213c2e9da8770c16bad17971f5148fd96612
advdylan <immortalizetimes@gmail.com>
2024-02-27 19:38:51 +0100

    nauka rest

--- backend/api/views.py
+++ backend/api/views.py
@@ -11,8 +11,11 @@ import json
 def api_home(request, *args, **kwargs):
     
     serializer = ProductSerializer(data=request.data)
-    if serializer.is_valid():
+    if serializer.is_valid(raise_exception=True):
+        #instance = serializer.save()
+        #instance = form.save()
         print(serializer.data)
-        data = serializer.data
-    return Response(data)
+       
+        return Response(serializer.data)
+    return Response({'invalid': 'not good data'}, status= 400)
   
