45906408827b7102033a6f3ff9ab72e732850124
advdylan <immortalizetimes@gmail.com>
2024-02-29 16:07:20 +0100

    123

--- backend/products/views.py
+++ backend/products/views.py
@@ -14,7 +14,9 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
         #serializer.save(user=self.request.user)
         print(serializer.validated_data)
         title = serializer.validated_data.get('title')
-        content = serializer.validated_data.get('content') or title
+        content = serializer.validated_data.get('content') or None
+        if content is None:
+            content = title
         
         serializer.save(content=content)
 
--- py_client/detail.py
+++ py_client/detail.py
@@ -3,6 +3,6 @@ import requests
 
 endpoint = "http://localhost:8000/api/products/1/" 
 
-get_response = requests.get(endpoint, json={'title' : "ABC123",'content': 'hello world'}) 
+get_response = requests.get(endpoint) 
 
 print(get_response.json())
