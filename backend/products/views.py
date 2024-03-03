9d1660c88ef24fcd9c4e644633dfea4a59ced9ae
advdylan <immortalizetimes@gmail.com>
2024-03-03 17:48:55 +0100

    rest nauka

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, mixins, permissions
 
 from .models import Product
 from .serializers import ProductSerializer
@@ -9,6 +9,7 @@ from django.shortcuts import get_object_or_404
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         #serializer.save(user=self.request.user)
@@ -49,6 +50,30 @@ class ProductDestroyAPIView(generics.DestroyAPIView):
         super().perform_destroy(instance)
 
 product_delete_view = ProductDestroyAPIView.as_view()
+
+
+class ProductMixinView(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    generics.GenericAPIView
+    ):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
+
+    def get(self,request, *args, **kwargs):
+        print(args, kwargs)
+        pk = kwargs.get('pk')
+        if pk is not None:
+            return self.retrieve(request, *args, **kwargs)
+        return self.list(request, *args, **kwargs)
+    
+    def post(self, request, *args,**kwargs):
+        return self.create(request, *args, **kwargs)
+    #def post()
+
+product_mixin_view = ProductMixinView.as_view()
 
 
 
--- py_client/create.py
+++ py_client/create.py
@@ -5,7 +5,7 @@ endpoint = "http://localhost:8000/api/products/"
 
 data = {
     'title': 'This Field is done',
-    'price': 24.99
+    'price': 15.99
 }
 get_response = requests.post(endpoint, json=data) 
 print(get_response.json())
