e4796ecf7e6e267ec4e4af1109622ca3c240427d
advdylan <immortalizetimes@gmail.com>
2024-03-06 18:18:47 +0100

    rescik tut

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,11 +1,22 @@
 from rest_framework import serializers
+from rest_framework.reverse import reverse
+
 from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
     my_discount = serializers.SerializerMethodField(read_only=True)
+    edit_url = serializers.SerializerMethodField(read_only=True)
+    url = serializers.HyperlinkedIdentityField(
+        view_name = "product-detail",
+        lookup_field = 'pk'
+        )
+    email = serializers.EmailField(write_only = True)
     class Meta:
         model = Product
         fields = [
+            'email',
+            'url',
+            'edit_url',
             'pk',
             'title',
             'content',
@@ -13,6 +24,25 @@ class ProductSerializer(serializers.ModelSerializer):
             'sale_price',
             'my_discount'
         ]
+    
+    def create(self, validated_data):
+        #email = validated_data.pop('email')
+        obj = super().create(validated_data)
+        #print(email, obj)
+        return obj
+    
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title')
+        return instance
+
+
+    def get_edit_url(self, obj):
+        # return f"api/v2/products/{obj.pk}/"
+        request = self.context.get('request')
+        if request is None:
+            return None
+        
+        return reverse("product-edit", kwargs={"pk": obj.pk}, request=request)
 
     def get_my_discount(self, obj):
         if not hasattr(obj, 'id'):
--- backend/products/views.py
+++ backend/products/views.py
@@ -21,7 +21,9 @@ class ProductListCreateAPIView(
 
     def perform_create(self, serializer):
         #serializer.save(user=self.request.user)
-        print(serializer.validated_data)
+        #print(serializer.validated_data)
+        email = serializer.validated_data.pop('email')
+        print(email)
         title = serializer.validated_data.get('title')
         content = serializer.validated_data.get('content') or None
         if content is None:
