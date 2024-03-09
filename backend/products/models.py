74556716fa6a67ec1cdaf980eff0995d36b1b2ac
advdylan <immortalizetimes@gmail.com>
2024-03-09 09:58:22 +0100

    search rest

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,8 +1,29 @@
 from django.db import models
 from django.conf import settings
+from django.db.models import Q
 
 
 # Create your models here.
+
+class ProductQuerySet(models.QuerySet):
+    def is_public(self):
+        return self.filter(public=True)
+    
+    def search(self,query, user=None):
+        lookup = Q(title__icontains=query) | Q(content__icontains=query)
+        qs = self.is_public().filter(lookup)
+        if user is not None:
+            qs2 = self.filter(user=user).filter(lookup)
+            qs = (qs | qs2).distinct()
+        return qs
+        
+class ProductManager(models.Manager):
+    def get_queryset(self, *args,**kwargs):
+        return ProductQuerySet(self.model, using=self._db)
+
+    def search(self,query,user=None):
+        return self.get_queryset().search(query, user=user)
+
 
 
 User = settings.AUTH_USER_MODEL #auth.user
@@ -11,6 +32,9 @@ class Product(models.Model):
     title = models.CharField(max_length = 120)
     content = models.TextField(blank = True, null=True)
     price = models.DecimalField(max_digits = 15, decimal_places = 2, default = 99.99)
+    public = models.BooleanField(default = True)
+
+    objects = ProductManager()
 
     @property
     def sale_price(self):
@@ -18,3 +42,5 @@ class Product(models.Model):
     
     def get_discount(self):
         return '123'
+    
+  
