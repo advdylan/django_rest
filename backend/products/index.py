a5f33d61317e283e2c03cf5f8e86ee79512ca0c6
advdylan <immortalizetimes@gmail.com>
2024-03-10 21:10:11 +0100

    rest

--- backend/products/index.py
+++ backend/products/index.py
@@ -6,12 +6,16 @@ from .models import Product
 
 @register(Product)
 class ProductIndex(AlgoliaIndex):
-    should_index = 'is_public'
+    #should_index = 'is_public'
     fields = [
         'title',
         'content',
         'price',
         'public'
     ]
+    settings = {
+        'searchableAttributes': ['title', 'content'],
+        'attributesForFaceting': ['user', 'public' ]
+    }
     tags = 'get_tags_list'
 #algoliasearch.register(Product)
--- backend/search/views.py
+++ backend/search/views.py
@@ -2,9 +2,21 @@ from django.shortcuts import render
 from rest_framework import generics
 from products.models import Product
 from products.serializers import ProductSerializer
+from rest_framework.response import Response
+from . import client
 
 
-class SearchListView(generics.ListAPIView):
+class SearchListView(generics.GenericAPIView):
+    def get(self,request, *args, **kwargs):
+        query = request.GET.get('q')
+        tag = request.GET.get('tag') or None
+        if not query:
+            return Response('', status=400)
+        results = client.perform_search(query, tags=[tag])
+        return Response(results)
+    
+
+class SearchLisOldtView(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
