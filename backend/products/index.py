1f1d7402c175ab434a72ce61f173e402a345a662
advdylan <immortalizetimes@gmail.com>
2024-03-10 17:44:25 +0100

    rescik

--- backend/products/index.py
+++ backend/products/index.py
@@ -1,5 +1,16 @@
 import algoliasearch_django as algoliasearch
-
+from algoliasearch_django import AlgoliaIndex
+from algoliasearch_django.decorators import register
 from .models import Product
 
-algoliasearch.register(Product)
+
+@register(Product)
+class ProductIndex(AlgoliaIndex):
+    should_index = 'is_public'
+    fields = [
+        'title',
+        'content',
+        'price',
+        'public'
+    ]
+#algoliasearch.register(Product)
