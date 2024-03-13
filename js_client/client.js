687b5058be8cd4871336e338ae637b45c75562ea
advdylan <immortalizetimes@gmail.com>
2024-03-13 18:16:02 +0100

    wystarczy nauki

--- js_client/client.js
+++ js_client/client.js
@@ -1,4 +1,5 @@
 const loginForm = document.getElementById('login-form')
+const baseEndpoint = "http://localhost:8000/api"
 if (loginForm) {
     // handle this login form
     loginForm.addEventListener('submit', handleLogin )
@@ -6,5 +7,28 @@ if (loginForm) {
 
 function handleLogin(event){
     console.log(event)
-        event.preventDefault()
+    event.preventDefault()
+    const loginEndpoint = `${baseEndpoint}/token/`
+    let loginFormData = new FormData(loginForm)
+    let loginObjectData = Object.fromEntries(loginFormData)
+    //console.log(loginObjectData['username'], loginObjectData['password'])
+    const options = {
+        method: "POST",
+        headers: {
+            "ContentType": "application/json"
+        },
+        body: JSON.stringify(loginObjectData)
+    }
+
+    fetch(loginEndpoint, options) // Promise
+    .then(response =>{ //functions on promise
+        console.log(response)
+        return response.json()
+    })
+    .then (x =>{
+        console.log(x)
+    })
+    .catch(err=> {
+        console.log('err', err)
+    })
 }
