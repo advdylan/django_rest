ca9d6f05e6067171f7e7b6ad7a771ae44b83d90f
advdylan <immortalizetimes@gmail.com>
2024-03-16 08:01:47 +0100

    rest api

--- js_client/client.js
+++ js_client/client.js
@@ -1,5 +1,6 @@
 const loginForm = document.getElementById('login-form')
 const baseEndpoint = "http://localhost:8000/api"
+const contentContainer = document.getElementById('content-container')
 if (loginForm) {
     // handle this login form
     loginForm.addEventListener('submit', handleLogin )
@@ -25,13 +26,63 @@ function handleLogin(event){
     .then(response =>{ //functions on promise
         return response.json()
     })
-    .then(handleAuthData)
+    .then(authData =>{
+        handleAuthData(authData, getProductlist)
+    })
     .catch(err=> {
         console.log('err', err)
     })
 }
 
-function handleAuthData(dupa) {
-    localStorage.setItem('access', dupa.access) // key -> value pair
-    localStorage.setItem('refresh', dupa.refresh)
+function handleAuthData(authData, callback) {
+    localStorage.setItem('access', authData.access) // key -> value pair
+    localStorage.setItem('refresh', authData.refresh)
+    if (callback) {
+        callback()
+    }
 }
+
+function writeToContainer(data) {
+    if (contentContainer) {
+        contentContainer.innerHTML = "<pre>" + JSON.stringify(data, null, 4) + "</pre>"
+    }
+}
+
+function getFetchOptions(method, body) {
+    return {
+        method: method === null ? "GET": method,
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${localStorage.getItem('access')}`
+                },
+                body: body ? body: null
+            }
+}
+
+function isTokenNotValid(jsonData) {
+    if (jsonData.code && jsonData.code === "token_not_valid"){
+        //run a refresh token fetch
+        alert("Please login again")
+        return false
+    }
+    return true
+}
+
+function getProductlist() {
+    const endpoint = `${baseEndpoint}/products/`
+    const options = getFetchOptions()
+    fetch(endpoint, options)
+
+    .then(response => {
+        return response.json()
+    })
+
+    .then(data=> {
+        const ValidData = isTokenNotValid(data)
+        if (ValidData) {
+            writeToContainer(data)
+        }
+    })
+}
+
+getProductlist()
